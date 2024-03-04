f52403d9c2ffc0a07a9b4ebe0f37651bbaf726dc
Nicolò Boschi <boschi1997@gmail.com>
2024-03-04 21:31:39 +0100

    tests: add more llm&embeddings to the compat matrix

--- ragstack-ai-js-e2e-tests/tests/langchain/compatibilityRAG.test.ts
+++ ragstack-ai-js-e2e-tests/tests/langchain/compatibilityRAG.test.ts
@@ -1,50 +1,24 @@
-import {getRequiredEnv, getVectorStoreHandler} from '../config';
-import {AstraDBVectorStore, AstraLibArgs} from "@langchain/community/vectorstores/astradb";
+import {getRequiredEnv} from '../config';
 import {VectorStore} from "@langchain/core/vectorstores";
 import {LLM} from "@langchain/core/language_models/llms";
-import {EmbeddingsInterface} from "@langchain/core/embeddings";
-import {ChatOpenAI, OpenAIEmbeddings} from "@langchain/openai";
-import {BaseLanguageModel} from "@langchain/core/language_models/base";
-import {CassandraLibArgs, CassandraStore} from "@langchain/community/vectorstores/cassandra";
-import {randomUUID} from "node:crypto";
-import {VectorDatabaseTypeNotSupported} from "../vectorStore";
+import {AzureChatOpenAI, ChatOpenAI, OpenAIEmbeddings} from "@langchain/openai";
 import {runConversationalRag, runCustomRagChain} from "./RAGApplications";
-import {BaseListChatMessageHistory} from "@langchain/core/chat_history";
-import {AstraDBChatMessageHistory} from "@langchain/community/stores/message/astradb";
-import {CassandraChatMessageHistory} from "@langchain/community/stores/message/cassandra";
-import {AstraDB} from "@datastax/astra-db-ts";
+import {
+    AstraDBVectorStoreSupplier, CassandraVectorStoreSupplier,
+    EmbeddingsInfoSupplier,
+    EnvDependantEmbeddings,
+    EnvDependantLLM,
+    LLMSupplier,
+    VectorStoreSupplier
+} from "./providers";
+import {AzureOpenAIEmbeddings} from "@langchain/azure-openai";
+import * as fs from "fs";
+import {GoogleVertexAIEmbeddings} from "@langchain/community/embeddings/googlevertexai";
+import {ChatGoogleVertexAI} from "@langchain/community/chat_models/googlevertexai";
+import {BedrockChat} from "@langchain/community/chat_models/bedrock";
+import {BedrockEmbeddings} from "@langchain/community/embeddings/bedrock";
 
 describe("RAG pipeline compatibility", () => {
-
-    interface Skippable {
-        skip(): boolean;
-    }
-
-    interface Nameable {
-        name(): string;
-    }
-
-    interface EmbeddingsInfoSupplier extends Skippable, Nameable {
-        getEmbeddings(): EmbeddingsInterface;
-
-        getDimensions(): number;
-    }
-
-    interface LLMSupplier extends Skippable, Nameable {
-        getLLM(): BaseLanguageModel;
-    }
-
-
-    interface VectorStoreSupplier extends Skippable, Nameable {
-
-        initialize(embeddingsInfo: EmbeddingsInfoSupplier): Promise<VectorStore>;
-
-        newChatHistory(): Promise<BaseListChatMessageHistory>;
-
-        close(): Promise<void>;
-
-    }
-
     class RAGCombination {
         vectorStore: VectorStoreSupplier;
         embeddings: EmbeddingsInfoSupplier;
@@ -64,191 +38,113 @@ describe("RAG pipeline compatibility", () => {
     }
 
 
-    function astraDB(): VectorStoreSupplier {
-        return new class implements VectorStoreSupplier {
-            name(): string {
-                return "astradb";
-            }
+    const openAIEmbeddings = new EnvDependantEmbeddings(
+        "OPEN_AI_KEY",
+        "openai",
+        1536,
+        () => new OpenAIEmbeddings({
+            openAIApiKey: getRequiredEnv("OPEN_AI_KEY")
+        }))
 
-            skip(): boolean {
-                try {
-                    getVectorStoreHandler().getBaseAstraLibArgs();
-                } catch (e: unknown) {
-                    if (e instanceof VectorDatabaseTypeNotSupported) {
-                        return true
-                    }
-                    throw e
-                }
-                return false
-            }
+    const openAILLM = new EnvDependantLLM(
+        "OPEN_AI_KEY",
+        "openai",
+        () => new ChatOpenAI({openAIApiKey: getRequiredEnv("OPEN_AI_KEY"), temperature: 0.7})
+    )
+
+    const azureOpenAIEmbeddings = new EnvDependantEmbeddings(
+        ["AZURE_OPEN_AI_KEY", "AZURE_OPEN_AI_ENDPOINT"],
+        "azure openai",
+        1536,
+        () => new AzureOpenAIEmbeddings({
+        azureOpenAIApiKey: getRequiredEnv("AZURE_OPEN_AI_KEY"),
+        azureOpenAIApiVersion: "2023-05-15",
+        azureOpenAIEndpoint: getRequiredEnv("AZURE_OPEN_AI_ENDPOINT"),
+        azureOpenAIApiDeploymentName: "text-embedding-ada-002",
+        modelName: "text-embedding-ada-002"
+    }))
+
+    const azureOpenAILLM = new EnvDependantLLM(
+        ["AZURE_OPEN_AI_KEY", "AZURE_OPEN_AI_ENDPOINT"],
+        "azure openai",
+        () => new AzureChatOpenAI({
+            azureOpenAIApiKey: getRequiredEnv("AZURE_OPEN_AI_KEY"),
+            azureOpenAIApiVersion: "2023-05-15",
+            azureOpenAIBasePath: getRequiredEnv("AZURE_OPEN_AI_ENDPOINT"),
+            azureOpenAIApiDeploymentName: "gpt-35-turbo",
+        }))
 
 
-            async close(): Promise<void> {
-                await getVectorStoreHandler().afterTest()
-            }
-
-            async initialize(embeddingsInfo: EmbeddingsInfoSupplier): Promise<VectorStore> {
-                return getVectorStoreHandler().beforeTest()
-                    .then(async () => {
-
-                        const config: AstraLibArgs = {
-                            ...getVectorStoreHandler().getBaseAstraLibArgs(),
-                            collectionOptions: {
-                                vector: {
-                                    dimension: embeddingsInfo.getDimensions(),
-                                    metric: "cosine",
-                                },
-                            },
-                        }
-                        const store = new AstraDBVectorStore(embeddingsInfo.getEmbeddings(), config);
-                        return store.initialize()
-                            .then(() => store)
-                    })
-            }
-
-            async newChatHistory(): Promise<BaseListChatMessageHistory> {
-                const baseAstraLibArgs = getVectorStoreHandler().getBaseAstraLibArgs();
-                const client = new AstraDB(
-                    baseAstraLibArgs.token,
-                    baseAstraLibArgs.endpoint
-                );
-                const collectionName = baseAstraLibArgs.collection + "_chat_history";
-                await client.createCollection(collectionName)
-                const collection = await client.collection(collectionName)
-                const history = new AstraDBChatMessageHistory(
-                    {
-                        collection,
-                        sessionId: randomUUID()
-                    }
-                );
-                return Promise.resolve(history)
-            }
-
+    function vertexSetup() {
+        if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+            process.env["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/gcloud-account-key.json"
+            fs.writeFileSync("/tmp/gcloud-account-key.json", process.env["GCLOUD_ACCOUNT_KEY_JSON"] as string)
         }
     }
 
-    function cassandra(): VectorStoreSupplier {
-        return new class implements VectorStoreSupplier {
-            store?: CassandraStore
-
-            name(): string {
-                return "cassandra";
-            }
-
-            skip(): boolean {
-
-                try {
-                    getVectorStoreHandler().getBaseCassandraLibArgs(1);
-                    return false
-                } catch (e: unknown) {
-                    if (e instanceof VectorDatabaseTypeNotSupported) {
-                        return true
-                    }
-                    throw e
-                }
-            }
-
-            async close(): Promise<void> {
-                await getVectorStoreHandler().afterTest()
-                if (this.store) {
-                    const client = await this.store.getCassandraTable().getClient();
-                    await client.shutdown()
-                    this.store = undefined
-                }
-            }
-
-            async initialize(embeddingsInfo: EmbeddingsInfoSupplier): Promise<VectorStore> {
-                await getVectorStoreHandler().beforeTest()
-                const config: CassandraLibArgs = {
-                    ...getVectorStoreHandler().getBaseCassandraLibArgs(embeddingsInfo.getDimensions()),
-                    primaryKey: [{name: "id", type: "uuid", partition: true}],
-                    // with metadata won't work, see https://github.com/langchain-ai/langchainjs/pull/4516
-                    metadataColumns: [{name: "metadata", type: "text"}]
-                }
-                this.store = new CassandraStore(embeddingsInfo.getEmbeddings(), config);
-                return this.store
-            }
-
-            newChatHistory(): Promise<BaseListChatMessageHistory> {
-                const baseCassandraLibArgs = getVectorStoreHandler().getBaseCassandraLibArgs(0);
-                return Promise.resolve(new CassandraChatMessageHistory({
-                    serviceProviderArgs: baseCassandraLibArgs.serviceProviderArgs,
-                    contactPoints: baseCassandraLibArgs.contactPoints,
-                    localDataCenter: baseCassandraLibArgs.localDataCenter,
-                    credentials: baseCassandraLibArgs.credentials,
-                    keyspace: baseCassandraLibArgs.keyspace,
-                    table: baseCassandraLibArgs.table + "_chat_history",
-                    sessionId: randomUUID()
-                }));
-            }
-
-
+    const vertexLLM = new EnvDependantLLM(
+        ["GCLOUD_ACCOUNT_KEY_JSON"],
+        "vertex",
+        () => {
+            vertexSetup();
+            return new ChatGoogleVertexAI()
         }
-    }
+    )
 
-    class EnvDependantSkippable implements Skippable {
-        readonly keys: string | string[];
-
-
-        constructor(keys: string | string[]) {
-            this.keys = keys;
+    const vertexEmbeddings = new EnvDependantEmbeddings(
+        ["GCLOUD_ACCOUNT_KEY_JSON"],
+        "vertex",
+        768,
+        () => {
+            vertexSetup();
+            return new GoogleVertexAIEmbeddings()
         }
+    )
 
-        protected getKey(key: string): string {
-            return getRequiredEnv(key)
+    const bedrockAnthropicLLM = new EnvDependantLLM(
+        ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "BEDROCK_AWS_REGION"],
+        "bedrock anthropic",
+        () => {
+            return new BedrockChat({
+                model: "anthropic.claude-v2",
+                region: getRequiredEnv("BEDROCK_AWS_REGION"),
+            })
         }
+    )
 
-        skip(): boolean {
-            let kk: string[]
-            if (!Array.isArray(this.keys)) {
-                kk = [this.keys]
-            } else {
-                kk = this.keys
-            }
-            for (const key of kk) {
-                if (!process.env[key]) {
-                    return true
-                }
-            }
-            return false
+    const bedrockMetaLLM = new EnvDependantLLM(
+        ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "BEDROCK_AWS_REGION"],
+        "bedrock anthropic",
+        () => {
+            return new BedrockChat({
+                model: "meta.llama2-13b-chat-v1",
+                region: getRequiredEnv("BEDROCK_AWS_REGION"),
+            })
         }
-    }
+    )
 
-    function openAIEmbeddings(): EmbeddingsInfoSupplier {
-        return new class extends EnvDependantSkippable implements EmbeddingsInfoSupplier {
+    const bedrockTitanEmbeddings = new EnvDependantEmbeddings(
+        ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "BEDROCK_AWS_REGION"],
+        "bedrock titan",
+        1536,
+        () => {
+            return new BedrockEmbeddings({
+                model: "amazon.titan-embed-text-v1",
+                region: getRequiredEnv("BEDROCK_AWS_REGION"),
+            })
+        })
 
-            constructor() {
-                super("OPEN_AI_KEY");
-            }
+    const bedrockCohereEmbeddings = new EnvDependantEmbeddings(
+        ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "BEDROCK_AWS_REGION"],
+        "bedrock cohere",
+        1024,
+        () => {
+            return new BedrockEmbeddings({
+                model: "cohere.embed-english-v3",
+                region: getRequiredEnv("BEDROCK_AWS_REGION"),
+            })
+        })
 
-            name(): string {
-                return "openai";
-            }
-
-            getDimensions(): number {
-                return 1536;
-            }
-
-            getEmbeddings(): EmbeddingsInterface {
-                return new OpenAIEmbeddings({openAIApiKey: this.getKey("OPEN_AI_KEY")});
-            }
-        }
-    }
-
-    function openAILLM(): LLMSupplier {
-        return new class extends EnvDependantSkippable implements LLMSupplier {
-            constructor() {
-                super("OPEN_AI_KEY");
-            }
-
-            name(): string {
-                return "openai";
-            }
-            getLLM(): BaseLanguageModel {
-                return new ChatOpenAI({openAIApiKey: this.getKey("OPEN_AI_KEY"), temperature: 0.7})
-            }
-        }
-    }
 
 
     class EmbeddingsLLMPair {
@@ -266,11 +162,16 @@ describe("RAG pipeline compatibility", () => {
         "conversational rag"
     ]
     const vectorStores: Array<VectorStoreSupplier> = [
-        astraDB(),
-        cassandra()
+        new AstraDBVectorStoreSupplier(),
+        new CassandraVectorStoreSupplier()
     ]
     const embeddingsLLM: Array<EmbeddingsLLMPair> = [
-        new EmbeddingsLLMPair(openAIEmbeddings(), openAILLM())
+        {embeddings: openAIEmbeddings, llm: openAILLM},
+        {embeddings: azureOpenAIEmbeddings, llm: azureOpenAILLM},
+        {embeddings: vertexEmbeddings, llm: vertexLLM},
+        {embeddings: bedrockTitanEmbeddings, llm: bedrockAnthropicLLM},
+        // cohere is broken
+        // {embeddings: bedrockCohereEmbeddings, llm: bedrockMetaLLM}
     ]
     const ragCombinations: Array<RAGCombination> = []
     const ragCombinationsToSkip: Array<RAGCombination> = []
@@ -294,6 +195,8 @@ describe("RAG pipeline compatibility", () => {
     if (ragCombinations.length) {
 
         test.each<RAGCombination>(ragCombinations)('Test %s', async (combination: RAGCombination) => {
+            console.log(await combination.llm.getLLM().invoke("Hello, world!", {}))
+            // console.log(await combination.embeddings.getEmbeddings().embedQuery("Hello, world!"))
             const llm: LLM = combination.llm.getLLM() as LLM
             try {
                 const vectorStore: VectorStore = await combination.vectorStore.initialize(combination.embeddings)
