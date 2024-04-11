209d014027427e9d9e6fb09d0cc5ac58b6a7f03c
Nicolò Boschi <boschi1997@gmail.com>
2024-04-11 18:15:26 +0200

    expose ragstack-version

--- packages/ragstack-ai-ts/tests/unit/import.test.ts
+++ packages/ragstack-ai-ts/tests/unit/import.test.ts
@@ -2,9 +2,14 @@ import {expect, test} from "@jest/globals";
 import {AstraDBVectorStore} from "@langchain/community/vectorstores/astradb";
 import {AstraDB} from "@datastax/astra-db-ts";
 import {OpenAI} from "@langchain/openai";
+import {RAGSTACK_VERSION} from "../../src";
 
 
 describe("Package tests", () => {
+
+    test('version test', async () => {
+        expect(RAGSTACK_VERSION).toBeTruthy()
+    })
 
     test('test import', async () => {
         expect(AstraDBVectorStore.lc_name()).toBe("AstraDBVectorStore")
