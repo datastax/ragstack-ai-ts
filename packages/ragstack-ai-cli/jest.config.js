11bf0bd7cb1a44d3eb2bda5b53cc5b087b46e293
Nicolò Boschi <boschi1997@gmail.com>
2024-04-12 10:24:47 +0200

    cli improvements and deps fixes (#17)

--- packages/ragstack-ai-cli/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-/* eslint-disable-next-line */
-module.exports = {
-  preset: 'ts-jest',
-  testTimeout: 180000,
-  setupFiles: ["dotenv/config"],
-  detectOpenHandles: true,
-}
